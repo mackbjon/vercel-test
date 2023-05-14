314b505eac2f1d57b1316ce8485569ecb94164dd
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-13 20:54:22 -0400

    Prisma data proxy working

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  title: 'Vercel Postgres Demo with Prisma',
+  title: 'pg-test',
   description:
     'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
 }
