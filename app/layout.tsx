67a211727664a6a0fc062a48a10b43792037bda2
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-17 19:16:18 -0400

    switch dynamic to error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 // export const runtime = 'edge'
-export const dynamic = 'force-static'
+export const dynamic = 'error'
 export const revalidate = 60
 
 export const metadata = {
