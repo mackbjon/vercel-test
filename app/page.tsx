867ba173b23b9dd0fbbdf29e8515faebd46d1ecb
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-18 03:05:55 -0400

    Add edge to correct table page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import NavButton from '../components/nav-button'
 
-export const runtime = 'edge'
-export const preferredRegion = 'iad1'
-
 export default function RootPage() {
   // You can add any UI inside Loading, including a Skeleton.
   return <NavButton />
--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,5 +1,8 @@
 import TableComp from '../../components/table'
 
+export const runtime = 'edge'
+export const preferredRegion = 'iad1'
+
 export default function Table() {
   // You can add any UI inside Loading, including a Skeleton.
 
