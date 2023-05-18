f7c1c919188e5fc6dc2507a219537dfbd5ba5044
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-18 16:30:02 -0400

    Change table page to async function

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -3,9 +3,10 @@ import TableComp from '../../components/table'
 export const runtime = 'edge'
 export const preferredRegion = 'iad1'
 
-export default function Table() {
+export default async function Table() {
   // You can add any UI inside Loading, including a Skeleton.
 
   /* @ts-expect-error Async Server Component */
+
   return <TableComp />
 }
