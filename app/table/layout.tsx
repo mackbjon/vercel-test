d4ff025216ccb171c04254375cf64d4514f32e21
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-18 21:21:59 -0400

    Change fetch cache to no store

--- app/table/layout.tsx
+++ app/table/layout.tsx
@@ -1,5 +1,6 @@
 export const runtime = 'edge'
 export const revalidate = 0
+export const fetchCache = 'force-no-store'
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
